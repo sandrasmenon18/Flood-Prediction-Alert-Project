b0cc9ecaf5cd0e5b9a280b7447fe8f23b97e2c4f
sandrasmenon18 <sandrasasirekha123@gmail.com>
2021-06-15 12:33:02 +0530

    added realtime data as default value in rainfall field and achanged some requirements

--- app.py
+++ app.py
@@ -9,16 +9,33 @@ model=pickle.load(open('model.pkl','rb'))
 
 @app.route('/')
 def hello_world():
-    return render_template("index.html")
+    import requests,bs4
+    res=requests.get("https://www.worldweatheronline.com/nileshwar-weather/kerala/in.aspx")
+    soup=bs4.BeautifulSoup(res.text,'lxml')
+    para=soup.select('p')
+    print(para[4].getText())
+    lis=para[4].getText().split()
+    print(lis)
+    rain=float(lis[1])
+    return render_template("index.html",r=rain)
 
 
 @app.route('/predict',methods=['POST','GET'])
 def predict():
     int_features=[float(x) for x in request.form.values()]
     print(int_features)
-    # import requests,bs4
-    # res=requests.get("https://www.worldweatheronline.com/nileshwar-weather/kerala/in.aspx")
-    # soup=bs4.BeautifulSoup(res.text,'lxml')
+    import requests,bs4
+    res=requests.get("https://www.worldweatheronline.com/nileshwar-weather/kerala/in.aspx")
+    soup=bs4.BeautifulSoup(res.text,'lxml')
+    para=soup.select('p')
+    print(para[4].getText())
+    lis=para[4].getText().split()
+    print(lis)
+    rain=float(lis[1])
+    int_features[0]=rain
+    # rese=requests.get("https://indiawris.gov.in/wris/#/evapotranspiration",verify=False)
+    # print(rese.text)
+    # # soup=bs4.BeautifulSoup(rese.text,'lxml')
     # para=soup.select('p')
     # print(para[4].getText())
     # lis=para[4].getText().split()
@@ -33,13 +50,13 @@ def predict():
     # output=float(output)
     print(type(output))
     if output>=str(4501):
-        return render_template('flood_pred.html',pred='Your region is in Danger',sel="alert")
+        return render_template('flood_pred.html',pred='Your region is in Danger',sel="alert",r=rain)
     elif output>=str(0) and output<=str(2500):
-        return render_template('flood_pred.html',pred='Your region is Safe',sel="alert1")
+        return render_template('flood_pred.html',pred='Your region is Safe',sel="alert1",r=rain)
     elif output>=str(2501) and output<=str(3500):
-        return render_template('flood_pred.html',pred='Your region is in Slight Danger.Watch and Be updated!!!',sel="alert2")
+        return render_template('flood_pred.html',pred='Your region is in Slight Danger.Watch and Be updated!!!',sel="alert2",r=rain)
     elif output>=str(3501) and output<=str(4500):
-        return render_template('flood_pred.html',pred='Be Prepared!!Your region is at risk.',sel="alert3")
+        return render_template('flood_pred.html',pred='Be Prepared!!Your region is at risk.',sel="alert3",r=rain)
 
 
 if __name__ == '__main__':
